8277cbbc6fe5ce27b223fee8948534f71cb06ef3
nzosim <nzosim54@gmail.com>
2024-03-08 11:32:08 +0100

    all

--- src/components/ArtistComponent.vue
+++ src/components/ArtistComponent.vue
@@ -1,15 +1,65 @@
 <script>
+import axios from 'axios';
+
 export default {
+  data() {
+        return {
+            errored: false,
+            loading: true,
+            infoBrute: null,
+            info: null
+        }
+    },
+    created() {
+        axios
+            .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+            .then(response => {
+                this.infoBrute = response.data
+                this.loading = false
+            })
+            .catch(error => {
+                console.log("erreur : " + error)
+                this.loading = false
+                this.errored = true
+            })
+            .finally(() => {
+                // méthode qui est appelé quand le callback d'une promesse est terminé
+                // resolve ou reject, cela évite de duppliquer du code dans le then et le catch
+                this.loading = false
+            })
+    }
 }
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <h1>Artiste</h1>
     <nav>
         
       </nav>
-  </div>
+  </div> -->
+  <h1>Méthode de workflow avec Axios</h1>
+    
+    <div id="container">
+        
+        <section v-if="errored">
+            <p>Désolé, nous n'avons pas pu accéder au serveur de données pour le moment, réessayez plus tard</p>
+        </section>
+
+        <section v-else>
+            <div v-if="loading">Chargement en cours ...</div>
+
+            <div v-else>
+                <h3>Valeur du Bitcoin</h3>
+                <p>La valeur du Bitcoin est de :</p>
+                <p>United States Dollar : {{infoBrute.bpi.USD.rate}} $</p>
+                <p>Euro : {{infoBrute.bpi.EUR.rate}} €</p>
+                <p>British Pound Sterling : {{infoBrute.bpi.GBP.rate}} £</p>
+                
+            </div>
+        </section>
+
+    </div>
 </template>
 
 <style>
--- src/components/MusicComponent.vue
+++ src/components/MusicComponent.vue
@@ -1,9 +1,14 @@
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
+      errored: false,
+      loading: true,
+      infoBrute: null,
       search: '',
-      music: []
+      musics: []
     }
   },
   methods: {
@@ -11,14 +16,31 @@ export default {
      * méthode searchMusic qui permet de rechercher une musique depuis l'API MusicBrainz
      */
     searchMusic() {
-      let url = `https://musicbrainz.org/ws/2/recording?query=${this.search}&fmt=json`;
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          this.music = data.recordings;
-          console.log(this.music);
+      // let research = this.search.split(' ').join('+');
+      // let url = `https://musicbrainz.org/ws/2/recording?query=${research}&fmt=json`;
+      // fetch(url)
+      //   .then(response => response.json())
+      //   .then(async data => {
+      //     this.music = await data;
+      //   })
+      //   .catch(error => console.error(error));
+      // console.log(this.music.recordings)
+      let research = this.search.split(' ').join('+');
+      let url = `https://musicbrainz.org/ws/2/recording?query=${research}&fmt=json`;
+      axios
+        .get(url)
+        .then(response => {
+            this.infoBrute = response.data
         })
-        .catch(error => console.error(error));
+        .catch(error => {
+            console.log("erreur : " + error)
+            this.errored = true
+        })
+        .finally(() => {
+          this.loading = false
+            this.musics = this.infoBrute.recordings
+            console.log(this.infoBrute.recordings)
+        })
     }
   }
 }
@@ -31,9 +53,19 @@ export default {
     <button @click="searchMusic">Rechercher</button>
   </div>
 
-  <div v-if="music.length > 0" v-for="test in music">
-    {{ test.title }} {{ test.first-release-date }}
-  </div>
+  <section v-if="errored">
+      <p>Désolé, nous n'avons pas pu accéder au serveur de données pour le moment, réessayez plus tard</p>
+  </section>
+
+  <section v-else>
+      <div v-if="loading">Chargement en cours ...</div>
+
+      <div v-else>
+        <div v-for="music in musics" :key="music.id">
+          {{ music['artist-credit'][0].name }} ** {{ music.title }}
+        </div>
+      </div>
+  </section>
 </template>
 
 <style>
